6a6e38b481e28283d98d5cc7c2d1ca16a8631f80
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2023-05-11 19:38:07 -0300

    Html Meta Tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <meta name="theme-color" content="#EEC77E" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#EEC77E" />
+        <meta name="msapplication-navbutton-color" content="#EEC77E" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="O Melhor Para O Seu Pet!" />
+
+        <title>França Vet &#128062;</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
