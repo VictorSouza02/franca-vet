c8abbcacf92581d031823e060a37812b94671830
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2023-05-12 19:33:28 -0300

    new import for storeBanner

--- src/pages/loja/index.jsx
+++ src/pages/loja/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/FooterResponsive";
-import StoreBanner from "./components/StoreBanner";
+import StoreBanner from "@/components/StoreBanner";
 
 export default function Store() {
     const [screen, setScreen] = useState(null)
