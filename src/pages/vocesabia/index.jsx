773b717f9b6131199f331475a8e0eab5dcd92c2e
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2023-05-13 14:00:01 -0300

    container tips fixed

--- src/pages/vocesabia/index.jsx
+++ src/pages/vocesabia/index.jsx
@@ -18,7 +18,7 @@ export default function Tips() {
               w-full overflow-hidden flex flex-col shadow-lg min-h-[259px]
           "
       >
-        <div className="w-[90%] md:w-[70%] h-full mx-auto mt-16 mb-4 flex flex-col gap-6">
+        <div className="w-[90%] md:w-[70%] max-w-[1000px] h-full mx-auto mt-16 mb-4 flex flex-col gap-6">
           <div>
             <h2 className="text-black-gray">Você Sabia?</h2>
             <h1 className="text-dark-blue text-2xl">Tudo sobre o mundo dos Pets!</h1>
