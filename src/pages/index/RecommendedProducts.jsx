bf484a770691184221b85d5de3c5b8dbdc330eda
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2023-05-17 18:22:36 -0300

    finished bath and grooming section

--- src/pages/index/RecommendedProducts.jsx
+++ src/pages/index/RecommendedProducts.jsx
@@ -162,7 +162,7 @@ export default function RecommendedProducts() {
                         width="16"
                         height="16"
                         viewBox="0 0 16 16"
-                        className="group-hover:fill-white duration-[0.6s]"
+                        className="group-hover:fill-white fill-dark-blue duration-[0.6s]"
                     >
                         <path fllrule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 
                         0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
